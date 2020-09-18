b44f71aba490394b60008d3311bdedace656dbb4
jeandobre <jeandobre@ufgd.edu.br>
2020-09-18 08:41:54 -0400

    Envio de mail no caso de uso

--- src/useCases/CreateUser/CreateUser.ts
+++ src/useCases/CreateUser/CreateUser.ts
@@ -1,3 +1,4 @@
+import { MailProvider } from './../../providers/MailProvider';
 import { User } from './../../entities/User';
 import { CreateUserRequestDTO } from './CreateUserDTO';
 import { UserRepository } from './../../repositories/UserRepository';
@@ -5,7 +6,8 @@ import { UserRepository } from './../../repositories/UserRepository';
 export class CreateUser {
 
 	constructor(
-		private userRepository: UserRepository
+		private userRepository: UserRepository,
+		private mailProvider: MailProvider
 	){}
 
 	async execute(data: CreateUserRequestDTO) {
@@ -18,5 +20,18 @@ export class CreateUser {
 		const user = new User(data);
 
 		await this.userRepository.save(user);
+
+		this.mailProvider.sendMail({
+			to: {
+				name: data.name,
+				email: data.email
+			},
+			from: {
+				name: 'Equipe do App FCC',
+				email: 'equipe@app.com'
+			},
+			subject: 'Bem vindo ao sistema',
+			body: '<p>Você já pode realizar o login</p>'
+		})
 	}
 }
